fcb53ea695becd8fb5952c0a998f07d1852a922a
shonageorge <shonageorge27@gmail.com>
2019-05-29 11:48:32 +0530

    final commit

--- trackService/src/main/java/com/stackroute/userservice/MusicApplication.java
+++ trackService/src/main/java/com/stackroute/userservice/MusicApplication.java
@@ -1,11 +1,7 @@
 package com.stackroute.userservice;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MusicApplication {
@@ -15,18 +11,4 @@ public class MusicApplication {
         SpringApplication.run(MusicApplication.class,args);
     }
 
-
-}
-@RefreshScope
-@RestController
-class MuzixRestController
-{
-    @Value("${msg:Hello World-Config Server is not working...please check}")
-    private String msg;
-
-    @RequestMapping("/msg")
-    String getMsg()
-    {
-        return this.msg;
-    }
 }
