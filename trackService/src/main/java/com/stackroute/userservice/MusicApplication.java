a7e071d887c65aa26b6a0d25362c660c1f996aba
Lasimasn <lasisn13@in.ibm.com>
2019-05-29 13:07:52 +0530

    updated user-service

--- trackService/src/main/java/com/stackroute/userservice/MusicApplication.java
+++ trackService/src/main/java/com/stackroute/userservice/MusicApplication.java
@@ -1,7 +1,11 @@
 package com.stackroute.userservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
@@ -12,5 +16,20 @@ public class MusicApplication {
         SpringApplication.run(MusicApplication.class,args);
         System.out.println();
     }
+
+}
+@RefreshScope
+@RestController
+class MsgRestController
+{
+    @Value("${msg:Hello}")
+    private String msg;
+
+    @RequestMapping("/msg")
+    String getMsg()
+    {
+        return this.msg;
+    }
+
 
 }
